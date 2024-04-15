d47902b65417983431183ec95dbc2029ce9f38d7
Kevin6969696969 <160507577+Kevin6969696969@users.noreply.github.com>
2024-04-15 11:47:54 -0300

    Add files via upload

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,10 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-for(let i=0;i <botoes.length;i++){
-    botoes[i].onclick = function(){
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
 
-        for(let j=0;j<botoes.length;j++){
+        for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
@@ -17,8 +17,8 @@ for(let i=0;i <botoes.length;i++){
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
 const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo4 = new Date("2023-02-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
@@ -41,12 +41,12 @@ function calculaTempo(tempoObjetivo) {
     }
 }
 
-function atualizaCronometro() {
+function atualizaCronometro(){
     for (let i=0; i<contadores.length;i++){
-        document.getElementById("dias"+i).textContent = calculaTempo(tempos [i])[0];
-        document.getElementById("horas"+i).textContent = calculaTempo(tempos [i])[1];
-        document.getElementById("min"+i).textContent = calculaTempo(tempos [i])[2];
-        document.getElementById("seg"+i).textContent = calculaTempo(tempos [i])[3];
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];   
     }
 }
 
